59834e034d2823db338445609ac504a8af992554
speak-mentaiko <1717miyawaki@gmail.com>
2024-08-25 19:37:22 +0900

    feat:viewの追加

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import type { Express } from "express";
 import contentsController from "./controllers/contents";
 
@@ -8,6 +9,13 @@ const app: Express = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.get('/', (req, res) => {
+  res.render('index'); // "index.ejs"をレンダリング
+});
 
 app.use("/contents", contentsController);
 
